8aa43e569c95ec763bf0caf31d1f25d3114f0498
Maxim harvančík <maximharvancik@gmail.com>
2025-10-30 21:19:30 +0100

    experiment n.20

--- app/dashboard/widgets/notes/page.tsx
+++ app/dashboard/widgets/notes/page.tsx
@@ -7,7 +7,7 @@ import Header from '@/components/sideHeader';
 
 // Initialize Supabase client
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 const supabase = createClient(supabaseUrl, supabaseKey);  
 
 interface Note {
