93dbf9702ef273de9774df8924d5fb58bf027527
Maxim harvančík <146641020+Max6453@users.noreply.github.com>
2025-10-31 11:57:40 +0100

    Updated redirecting link

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function Example() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="/dashboard"
+                    href="/auth/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
                   >
                     Log in
